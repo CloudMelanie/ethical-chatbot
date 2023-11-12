32d266918482e490c0905e6f5142097809569a9a
Melanie Cloud <mlcloud@smu.edu>
2023-11-11 22:58:38 -0600

    Made the buttons work (hopefully)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-#from openai import OpenAI
+# from openai import OpenAI
 import openai
 import streamlit as st
 import os
 
+
 # export OPENAI_API_KEY='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'
-openai.api_key= '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'
+openai.api_key = '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'
 # image load
 image = "C:/Users/melan/OneDrive/Documents/download.jpeg"
 
@@ -15,32 +16,45 @@ st.image(image)
 st.title("Ethical Chatbot")
 st.write("Welcome to our Ethical Chatbot Application. Enter a prompt: ")
 
-# Prompt user for input
+
 message = st.text_input("User: ", key="user_input")
 st.write("You asked: ", message)
 
-# Exit program if user inputs "quit"
 if message.lower() == "quit":
-  st.balloons()
+    st.balloons()
+    exit()
+
+if st.button("Positive", help="Provides an answer to the posed question from a positive position",
+             type="primary"):
+    st.write("Positive: ")
+    message += "Answer from a positive point of view"
+
+elif st.button("Negative", help="Provides an answer to the posed question from a negative position",
+               type="primary"):
+    st.write("Negative")
+    message += "Answer from a negative point of view"
+
+elif st.button("Neutral", help="Provides an answer to the posed question from a neutral position",
+               type="primary"):
+    st.write("Neutral")
+    message += "Answer from a negative point of view"
+# Prompt user for input
+
 
 else:
-  
-  if message: 
-    
-    # else put the message into the gpt engine and print response
-    ##client = OpenAI()
-
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
+    if message:
+        st.write("Chatbot: Thinking of a good response. Hang tight.........")
+        st.write("")
+        # else put the message into the gpt engine and print response
+        messages = [
             {"role": "user", "content": f"{message}"}
         ]
-    )
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+        answer = completion.choices[0].message.content
 
-    answer = completion.choices[0].message
-
-    st.write("Chatbot: Thinking of a good response. Hang tight.........")
-    st.write("")
-    
-    # Display the response
-    st.markdown(f"Chatbot: {answer}")
+        # Display the response
+        st.markdown(f"Chatbot: {answer}")
+        messages.append({"role": "assistant", "content": answer})
