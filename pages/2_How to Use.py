e9d52696d58b6ea37b7226e75bdd97eb23d1c053
laiharrington <111807688+laiharrington@users.noreply.github.com>
2023-11-14 20:09:31 -0600

    added return to Home button

--- pages/2_How to Use.py
+++ pages/2_How to Use.py
@@ -5,4 +5,12 @@ st.title("How to Use")
 
 st.subheader("1. Enter an ethical related question.")
 st.subheader("2. Select one of three stances: Positive, Neutral, or Negative.")
+container = st.container()
+container.markdown("<br>"*18, unsafe_allow_html=True)
 
+col1, col2, col3 = container.columns(3)
+
+# Add buttons to each column
+with col2:
+    if st.button("return to Home", type="primary"):
+        switch_page("Home")
