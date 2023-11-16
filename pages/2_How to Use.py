416e75a87c432041c6c4367ff8cfa1a635f90a4a
Briana <bcamillus@smu.edu>
2023-11-15 20:41:15 -0600

    fixed spacing

--- pages/2_How to Use.py
+++ pages/2_How to Use.py
@@ -6,7 +6,7 @@ st.title("How to Use")
 st.subheader("1. Enter an ethical related question.")
 st.subheader("2. Select one of three stances: Positive, Neutral, or Negative.")
 container = st.container()
-container.markdown("<br>"*18, unsafe_allow_html=True)
+container.markdown("<br>"*2, unsafe_allow_html=True)
 
 col1, col2, col3 = container.columns(3)
 
